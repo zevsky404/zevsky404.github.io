85013a925b8469d0c02c5e6569317235beeba952
zevsky404 <chrissiguetter2001@gmail.com>
2023-08-22 18:25:22 +0200

    add bootstrap imports, add first webpage structure, test automated building of cards for grid

--- resources/js/debug.js
+++ resources/js/debug.js
@@ -1,6 +1,6 @@
 // PRINT STATEMENTS FOR DEBUGGING
 
-import {getCompletePokedexData, buildPokemon, findPokemonByName, findPokemonByNumber} from "./utils";
+import {getCompletePokedexData, buildPokemon, findPokemonByName, findPokemonByNumber, buildCard} from "./utils";
 
 getCompletePokedexData.then((result) => {
     console.log(result[1]);
@@ -8,8 +8,20 @@ getCompletePokedexData.then((result) => {
     console.log(ivysaur);
 
     let arceus = findPokemonByName("Arceus", result);
+    let arceus1 = findPokemonByNumber("493", result);
     console.log(arceus)
+    console.log(arceus1)
     arceus = buildPokemon(arceus);
     console.log(arceus);
+
+    let cellList = document.getElementsByClassName("col-4 themed-grid-col");
+
+    let safetyCounter = 0;
+    for (let cell of cellList) {
+        let pokemon = buildPokemon(result[safetyCounter]);
+        cell.appendChild(buildCard(pokemon));
+        ++safetyCounter;
+    }
+
 })
 
--- resources/js/utils.js
+++ resources/js/utils.js
@@ -1,6 +1,7 @@
 // UTILITY FUNCTIONS USEFUL FOR THE ENTIRE PROJECT
 import * as d3 from "d3"
 import {Pokemon, BaseStats} from "./objects";
+import {easeQuadOut} from "d3";
 
 export let getCompletePokedexData = d3.csv("resources/data/pokemon.csv").then((response) => {
     return response;
@@ -56,6 +57,49 @@ export function buildPokemon(datasetInfo) {
     pokemon.generation = datasetInfo.generation;
     pokemon.isLegendary = !Boolean(datasetInfo.is_legendary);
 
-
     return pokemon;
+}
+
+export function buildCard(pokemon) {
+    let cardDiv = document.createElement("div");
+    cardDiv.className = "card";
+    cardDiv.style.width = "18rem";
+
+    let cardTitle = document.createElement("div");
+    cardTitle.className = "card-header";
+    cardTitle.innerText = `#${pokemon.number} - ${pokemon.name}`;
+
+    let image = document.createElement("img");
+    image.className = "card-img-top";
+    image.src = pokemon.image;
+
+    let cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+    cardBody.style.display = "flex";
+    cardBody.style.justifyContent = "center";
+
+    let iconHolderType1 = document.createElement("div");
+    iconHolderType1.className = `icon ${pokemon.type1}`;
+
+    let iconImageType1 = document.createElement("img");
+    iconImageType1.src = `resources/images/icons/${pokemon.type1}.svg`
+
+    iconHolderType1.appendChild(iconImageType1);
+    cardBody.appendChild(iconHolderType1);
+    cardDiv.appendChild(cardTitle);
+    cardDiv.appendChild(image);
+    cardDiv.appendChild(cardBody);
+
+    if (pokemon.type2) {
+        let iconHolderType2 = document.createElement("div");
+        iconHolderType2.className = `icon ${pokemon.type2}`;
+
+        let iconImageType2 = document.createElement("img");
+        iconImageType2.src = `resources/images/icons/${pokemon.type2}.svg`
+
+        iconHolderType2.appendChild(iconImageType2);
+        cardBody.appendChild(iconHolderType2);
+    }
+
+    return cardDiv;
 }
