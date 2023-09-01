0dedd2b1009e84151ba32ad1563ca8813408eab1
zevsky404 <chrissiguetter2001@gmail.com>
2023-09-01 12:10:48 +0200

    complete card compression on zoom

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,5 @@
 // MAIN FILE WHICH CALLS FUNCTIONS FOR EASE OF WORKFLOW
-import {getCompletePokedexData, buildPokemon, buildCard, shortenText, getTextNodeWidth} from "./utils";
+import {getCompletePokedexData, buildPokemon, buildCard} from "./utils";
 
 function buildIndexStructure() {
     getCompletePokedexData.then((data) => {
@@ -48,7 +48,31 @@ function zoomCards() {
         } else if (!card.children[0].textContent.includes(card.classList[3])) {
             card.children[0].textContent += ` - ${card.classList[3]}`;
         }
+
+        if (slider.value <= 4.5) {
+            const type1Colour = getComputedStyle(types[0]).backgroundColor;
+            let rgbValues1 = type1Colour.split(",");
+            rgbValues1[0] = rgbValues1[0].substring(4);
+            rgbValues1[2] = rgbValues1[2].slice(0,-1);
+
+            if (types[1]) {
+                const type2Colour = getComputedStyle(types[1]).backgroundColor;
+                let rgbValues2 = type2Colour.split(",");
+                rgbValues2[0] = rgbValues2[0].substring(4);
+                rgbValues2[2] = rgbValues2[2].slice(0,-1);
+                cardTitle.style.backgroundColor = `rgba(${rgbValues2[0]}, ${rgbValues2[1]}, ${rgbValues2[2]}, 0.58)`;
+            }
+            cardImage.style.backgroundColor = `rgba(${rgbValues1[0]}, ${rgbValues1[1]}, ${rgbValues1[2]}, 0.58)`;
+            cardBody.style.display = "none";
+        } else {
+            cardTitle.style.backgroundColor = "transparent";
+            cardImage.style.backgroundColor = "transparent";
+            if (cardBody.style.display === "none"){
+                cardBody.style.display = "flex";
+            }
+        }
     }
+
     
 }
 
