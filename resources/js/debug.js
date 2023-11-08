7ae46012097194ba2abcea54f81dce8ef6b7c581
zevsky404 <chrissiguetter2001@gmail.com>
2023-11-08 18:19:27 +0100

    fix 404 errors and file paths

--- resources/js/debug.js
+++ resources/js/debug.js
@@ -1,5 +1,5 @@
 // PRINT STATEMENTS FOR DEBUGGING
-import {getCompletePokedexData, buildPokemon, findPokemonByName, findPokemonByNumber, buildCard} from "/resources/js/utils";
+import {getCompletePokedexData, buildPokemon, findPokemonByName, findPokemonByNumber, buildCard} from "/resources/js/utils.js";
 
 function debugMain() {
     getCompletePokedexData.then((result) => {
